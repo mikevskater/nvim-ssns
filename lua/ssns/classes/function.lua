42dc09528805bfb8fd71b153f33fea2a0b04827a
mikevskater <mikevskater@hotmail.com>
2025-12-03 22:05:50 -0500

    refactor: update load_definition to use connection configuration object instead of connection string

--- lua/ssns/classes/function.lua
+++ lua/ssns/classes/function.lua
@@ -307,7 +307,7 @@ function FunctionClass:load_definition()
   local query = adapter:get_definition_query(db.db_name, self.schema_name, self.function_name, "FUNCTION")
 
   -- Execute query
-  local results = adapter:execute(self:get_server().connection_string, query, { use_delimiter = false })
+  local results = adapter:execute(self:get_server().connection_config, query, { use_delimiter = false })
 
   -- Use adapter's parse method for consistent result handling
   self.definition = adapter:parse_definition(results)
